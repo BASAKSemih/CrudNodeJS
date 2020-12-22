843e2f34b5b016f2418884a0b3a8f154c313ac10
BASAK Semih <semihbasak25@gmail.com>
2020-12-22 10:17:29 +0100

    some fix

--- routes/crud/add.js
+++ routes/crud/add.js
@@ -8,17 +8,16 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', async function (req, res, next) {
-    const { title, resume, genre } = req.body //  const username = req.body.username et  const password = req.body.password 
+    const { title, resume, genre } = req.body
 
     const film = await db.film.findOne({where : {title: title}})
     if (film){
 
-        res.render('register', { title: 'Express', error:'Ce film existe déjà' });
+        res.render('add', { title: 'Express', error:'Ce film existe déjà' });
         return
         
     }
     await db.film.create( {title: req.body.title, resume: req.body.resume, genre: req.body.genre} );
-    //res.redirect('login', {title: 'Vous pouvez vous connecter à présent'});
     res.render('home', { title: 'Express', error:'Film ajouter avec succès ' });
     
 
--- routes/crud/home.js
+++ routes/crud/home.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const db = require ('../../models/index')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('home', { title: 'Express' });
 });
 
+
+
+  
 module.exports = router;
